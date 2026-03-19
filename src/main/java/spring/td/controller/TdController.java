552db66365ab2f2f72ef534c70b5d28a338cd4f4
Hiraina <hei.hiraina@gmail.com>
2026-03-19 11:18:22 +0300

    feat(students): add POST /students route to store and return student names

--- src/main/java/spring/td/controller/TdController.java
+++ src/main/java/spring/td/controller/TdController.java
@@ -1,13 +1,26 @@
 package spring.td.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spring.td.entity.StudentEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class TdController {
     @GetMapping("/welcome")
     String sayWelcome(@RequestParam String name) {
         return "Welcome " + name;
+    }
+
+    List<StudentEntity> studentsList = new ArrayList<>();
+    @PostMapping("/students")
+    String saveStudents(@RequestBody List<StudentEntity> newStudents) {
+        studentsList.addAll(newStudents);
+        return studentsList
+                .stream()
+                .map(studentEntity -> studentEntity.getFirstName() + " " + studentEntity.getLastName())
+                .collect(Collectors.joining(", "));
     }
 }
