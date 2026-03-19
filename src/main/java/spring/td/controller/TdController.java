4eeeb65bac04e170a3b98a08c0f11db6d5153f0b
Hiraina <hei.hiraina@gmail.com>
2026-03-19 10:45:14 +0300

    feat: add GET /welcome endpoint with name query param

--- src/main/java/spring/td/controller/TdController.java
+++ src/main/java/spring/td/controller/TdController.java
@@ -1,7 +1,13 @@
 package spring.td.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TdController {
+    @GetMapping("/welcome")
+    String sayWelcome(@RequestParam String name) {
+        return "Welcome " + name;
+    }
 }
